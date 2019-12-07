eba9818c4abd8910f85ea0de3e7897803c2b4fd6
TYtrack <zzz1362194009@163.com>
2019-12-07 15:33:11 +0800

    initiate

---
+++ readme.txt
@@ -0,0 +1,2 @@
+Network management course
+
---
+++ 网络 管理/netMan.py
@@ -0,0 +1,14 @@
+﻿import redis
+import os
+import csv
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+with open('test2.csv')as f:
+    f_csv = csv.reader(f)
+    for row in f_csv:
+        print(row[0])
+        f=os.popen(row[0])
+        d=f.read()
+        print(d)
+        r.set(row[0],d)
